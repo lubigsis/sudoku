a16fdd853d65e9d98ff5b058b3d84f7d36d4b44e
lubigsis <lucre.baluc@gmail.com>
2023-03-10 10:55:24 -0300

    Aplico estilos-clases desde Javascript al tablero y otras funciones para comprobar errores/aciertos.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //---------------------------------------------------------declaro variables--------------------
 let numSeleccionado = null;
 let fichaSeleccionada = null;
-
 let errores = 0;
 
 let board = [
@@ -50,6 +49,30 @@ function selectNumero(){
     numSeleccionado = this;
     numSeleccionado.classList.add('selectNumero');
 }
+
+//------------función para q' se imprima en número en el tablero
+
+function selectFicha(){
+    if(numSeleccionado){
+        //para q' no se sobreescriba el nro. en el tablero.
+        if(this.innerText != ''){
+            return;
+        }
+        //compruebo coordenadas para ver si acertó: "0-0" "0-1"... "3-1"
+        let coords = this.id.split('-'); // ["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if(solucion[r][c] == numSeleccionado.id){
+            this.innerText = numSeleccionado.id;
+        }
+        else{
+            errores += 1;
+            document.getElementById('errores').innerText = errores;
+        }
+    }
+}
+
 //------------------------------------imprimo el tablero----------------------------------
 
 for (let r = 0; r < 9; r++) {
@@ -58,6 +81,18 @@ for (let r = 0; r < 9; r++) {
 
         let ficha = document.createElement("div");
         ficha.id = r.toString() + "-" + c.toString();
+        if(board[r][c] != '-'){
+            ficha.innerText = board[r][c]; //imprime todos los nros. en el tablero excepto los guiones.
+            ficha.classList.add('ficha-start');
+        }
+        if(r == 2 || r == 5){
+            ficha.classList.add('horizontal-line');  //CSS -> líneas que subdividen al tablero.
+        }
+        if(c == 2 || c == 5){
+            ficha.classList.add('vertical-line');
+        }
+        
+        ficha.addEventListener('click', selectFicha);
         ficha.classList.add("ficha");
         document.getElementById("tablero").append(ficha);
     }
